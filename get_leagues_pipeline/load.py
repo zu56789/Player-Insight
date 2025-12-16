5bb82dbd28856f36f40801c9a58d257b3caedf9e
Zuhayr <zuhayrmohsen@gmail.com>
2025-12-16 15:57:58 +0000

    Added function for loading league into rds

--- get_leagues_pipeline/load.py
+++ get_leagues_pipeline/load.py
@@ -41,3 +41,33 @@ def get_rds_connection() -> connection:
         host=secret_dict['DB_HOST']
     )
     return conn
+
+
+def upload_league_data(conn: connection, league_data: dict) -> bool:
+    """Upload league data to the PostgreSQL RDS database."""
+    check_query = """
+        SELECT * FROM league
+        WHERE league_name = %s AND league_season = %s;
+        """
+    with conn.cursor() as cursor:
+
+        cursor.execute(check_query, (
+            league_data['league_name'],
+            league_data['league_season']
+        ))
+        existing_league = cursor.fetchone()
+        if existing_league:
+            return False  # League data already exists
+
+        insert_query = """
+        INSERT INTO league (league_name, league_country, league_season, fbref_url)
+        VALUES (%s, %s, %s, %s);
+        """
+
+        cursor.execute(insert_query, (
+            league_data['league_name'],
+            league_data['league_country'],
+            league_data['league_season'],
+            league_data['fbref_url']
+        ))
+        return True
