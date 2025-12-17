2c411a81a4c01ccbef74bd9c4b95fe223e87e195
Zuhayr <zuhayrmohsen@gmail.com>
2025-12-17 18:55:49 +0000

    Added logic for getting league id by name

--- get_teams_pipeline/extract.py
+++ get_teams_pipeline/extract.py
@@ -42,7 +42,8 @@ def get_league_teams(url: str) -> list[dict]:
         if team_cell and "/squads/" in team_cell['href']:
             teams.append({
                 "team_name": team_cell.text.strip(),
-                "fbref_link": team_cell['href']
+                "fbref_link": team_cell['href'],
+                "league_name": league_name
             })
         else:
             raise ValueError("Team link not found in the expected format")
--- get_teams_pipeline/load.py
+++ get_teams_pipeline/load.py
@@ -16,3 +16,33 @@ def get_rds_connection() -> connection:
         host=os.getenv("DB_HOST")
     )
     return conn
+
+
+def get_league_id_for_league(conn: connection, league_name: str) -> int:
+    """Retrieve the league ID for a given league name."""
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT league_id FROM league WHERE league_name = %s;",
+            (league_name,)
+        )
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            raise ValueError(
+                f"League '{league_name}' not found in the database.")
+
+
+if __name__ == "__main__":
+    conn = get_rds_connection()
+    print("Connection to RDS database established successfully.")
+    league_names = ["Premier League", "La Liga",
+                    "Bundesliga", "Serie A", "Ligue 1"]
+    for league_name in league_names:
+
+        try:
+            league_id = get_league_id_for_league(conn, league_name)
+            print(f"League ID for '{league_name}': {league_id}")
+        except ValueError as e:
+            print(e)
+    conn.close()
