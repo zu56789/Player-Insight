784d00b9b1c6bc7ed0c53a23d588e76c096947dc
Zuhayr <zuhayrmohsen@gmail.com>
2026-01-01 16:45:29 +0000

    Added code for retrieving each teams name and their id and url

--- get_team_players_pipeline/load.py
+++ get_team_players_pipeline/load.py
@@ -16,3 +16,51 @@ def get_rds_connection() -> connection:
         host=os.getenv("DB_HOST")
     )
     return conn
+
+
+def get_team_id_for_team(conn: connection, team_name: str):
+    """Retrieve the team ID for a given team name."""
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT team_id FROM team WHERE team_name = %s;",
+            (team_name,)
+        )
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            raise ValueError(
+                f"Team '{team_name}' not found in the database.")
+
+
+def get_team_names(conn: connection) -> list[str]:
+    """Retrieve all team names from the team table."""
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT team_name FROM team;")
+        results = cursor.fetchall()
+        return [row[0] for row in results]
+
+
+def get_fbref_url_for_team(conn: connection, team_name: str) -> str:
+    """Retrieve the fbref URL for a given team name."""
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "SELECT fbref_url FROM team WHERE team_name = %s;",
+            (team_name,)
+        )
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            raise ValueError(
+                f"FBRef URL for team '{team_name}' not found in the database.")
+
+
+if __name__ == "__main__":
+    conn = get_rds_connection()
+    teams = get_team_names(conn)
+    for team in teams:
+        team_id = get_team_id_for_team(conn, team)
+        fbref_url = get_fbref_url_for_team(conn, team)
+        print(
+            f"Team ID is: {team_id}, Team name is {team}, fbref URL for team is: {fbref_url}")
